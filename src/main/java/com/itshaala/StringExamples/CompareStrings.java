350e8f845b86cd999b136ee65fbfc84c947eaa7f
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-20 17:34:46 +0530

    compare strings

---
+++ src/main/java/com/itshaala/StringExamples/CompareStrings.java
@@ -0,0 +1,14 @@
+package com.itshaala.StringExamples;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+        String string = "komal";
+        String string1 = "komal";
+
+        if(string.equals(string1)){
+            System.out.println("same");
+        }else{
+            System.out.println("not same");
+        }
+    }
+}
