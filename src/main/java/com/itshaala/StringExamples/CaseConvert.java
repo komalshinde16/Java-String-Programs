0dbd18ecd6ce0ee8bbd349a6966927c10ff24a95
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-20 17:30:11 +0530

    case convert

---
+++ src/main/java/com/itshaala/StringExamples/CaseConvert.java
@@ -0,0 +1,9 @@
+package com.itshaala.StringExamples;
+
+public class CaseConvert {
+    public static void main(String[] args) {
+        String string = "Komal";
+        System.out.println(string.toLowerCase());
+        System.out.println(string.toUpperCase());
+    }
+}
