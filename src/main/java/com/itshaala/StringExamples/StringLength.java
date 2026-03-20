8c480fbe21395b8667780d4b723588ced2244be3
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-20 17:28:47 +0530

    string length

---
+++ src/main/java/com/itshaala/StringExamples/StringLength.java
@@ -0,0 +1,8 @@
+package com.itshaala.StringExamples;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Komal";
+        System.out.println(str.length());
+    }
+}
