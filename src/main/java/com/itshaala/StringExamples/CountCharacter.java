8f6ac6e8ffb6964aedd7400eb4d23366bceb9dc6
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-23 11:21:42 +0530

    count

---
+++ src/main/java/com/itshaala/StringExamples/CountCharacter.java
@@ -0,0 +1,8 @@
+package com.itshaala.StringExamples;
+
+public class CountCharacter {
+    public static void main(String[] args) {
+        String string = "Strings examples";
+        System.out.println(string.length());
+    }
+}
