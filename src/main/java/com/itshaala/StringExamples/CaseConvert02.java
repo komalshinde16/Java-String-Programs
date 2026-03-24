67cc94300799f42e2e9a7de20a36825dc9e9d514
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-24 20:36:53 +0530

    i

---
+++ src/main/java/com/itshaala/StringExamples/CaseConvert02.java
@@ -0,0 +1,10 @@
+package com.itshaala.StringExamples;
+
+public class CaseConvert02 {
+    public static void main(String[] args) {
+        String string1 = "zomato";
+        String string2 = "blinkit";
+        System.out.println(string2.toUpperCase());
+        System.out.println(string1.toLowerCase());
+    }
+}
