d447b05ce475ba87d5bcfbc8deaee315d0163550
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-24 20:38:31 +0530

    count

---
+++ src/main/java/com/itshaala/StringExamples/CountCharacters02.java
@@ -0,0 +1,8 @@
+package com.itshaala.StringExamples;
+
+public class CountCharacters02 {
+    public static void main(String[] args) {
+        String string = "September";
+        System.out.println(string.length());
+    }
+}
