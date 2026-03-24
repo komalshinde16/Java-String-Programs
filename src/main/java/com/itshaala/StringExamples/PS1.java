c40a01e865d2970e951424d3b3577c95ca762261
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-24 20:13:46 +0530

    print string

---
+++ src/main/java/com/itshaala/StringExamples/PS1.java
@@ -0,0 +1,8 @@
+package com.itshaala.StringExamples;
+
+public class PS1 {
+    public static void main(String[] args) {
+        String string = "String examples";
+        System.out.println(string);
+    }
+}
