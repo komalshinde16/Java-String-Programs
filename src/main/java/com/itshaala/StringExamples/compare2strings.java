5897d466a03614f58aa7dc05af0911b48c42f6f3
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-24 20:43:15 +0530

    comparison

---
+++ src/main/java/com/itshaala/StringExamples/compare2strings.java
@@ -0,0 +1,14 @@
+package com.itshaala.StringExamples;
+
+public class compare2strings {
+    public static void main(String[] args) {
+        String string1 ="sir";
+        String string2 = "sir";
+
+        if(string1.equals(string2)){
+            System.out.println("same strings");
+        }else{
+            System.out.println("not the same strings");
+        }
+    }
+}
